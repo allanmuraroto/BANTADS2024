dd5fa61dba81be8d841faa8b898471b6f98c7796
allanmuraroto <allan.muraroto@hotmail.com>
2024-03-11 13:55:49 -0300

    iniciando tela inicial do gerente

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { GerenteComponent } from './gerente/gerente.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'gerente', component: GerenteComponent },
+  // Outras rotas podem ser adicionadas aqui, se necessário
+];
