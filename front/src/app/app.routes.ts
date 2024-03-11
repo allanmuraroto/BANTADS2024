a2020f37d256027895330df09e2b79588315bb46
LAPTOP-UOMTMQDU\Murilo Santos <Murilo dos Santos>
2024-03-11 19:36:53 -0300

    parcial tela melhores clientes gerente

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { MelhoresClientesentesComponent } from './gerente/melhores-clientesentes/melhores-clientesentes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{path: 'gerente', component: MelhoresClientesentesComponent}];
